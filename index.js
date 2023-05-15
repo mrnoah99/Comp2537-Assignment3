8f833c09bb4266acc6a36fea99ba81ed5f0599c4
Noah <nbaldwin3@my.bcit.ca>
2023-05-14 22:59:53 -0700

    Changed to use cards system like in the example as the way I was doing it was making it more difficult to implement pages and the modal together.

--- index.js
+++ index.js
@@ -1,33 +1,72 @@
+const PAGE_SIZE = 15;
+let currentPage = 1;
+let pokemons = []
+
+const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
+    let selected_pokemons = pokemons.slice((currentPage - 1) * PAGE_SIZE, currentPage * PAGE_SIZE);
+
+    $("#pokecards").empty();
+    selected_pokemons.forEach(async (pokemon) => {
+        const response = await axios.get(pokemon.url);
+        $("#pokecards").append(`
+            <div class='pokecard card' pokeName=${response.data.name}>
+                <h3>${response.data.name.toUpperCase()}</h3>
+                <img src='${response.data.sprites.front_default}' alt='${response.data.name}'>
+                <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#pokeModal'>
+                More...
+                </button>
+            </div>
+        `);
+    });
+    
+}
+
 const setup = async () => {
     let response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810");
-    // test connection to pokeapi by logging what we get
-    console.log(response.data.results);
-    console.log("First pokemon: " + response.data.results[0].name + ", " + response.data.results[0].url);
-    let i2 = 1;
-    let i3 = 1;
-    let nextRow = 4;
-    for (i = 0; i < 15; i++) {
-        let pokemon = await axios.get(response.data.results[i].url);
-        console.log(pokemon.data);
-        let a = document.createElement("div");
-        let b = document.createElement("img");
-        let c = document.createElement("br");
-        let d = document.createElement("span");
-        let e = document.getElementById(`r${i2}c${i3}`);
-        b.src = pokemon.data.sprites.front_default;
-        b.style = "width: 100px; height: auto;";
-        d.innerHTML = pokemon.data.name;
-        a.appendChild(b);
-        a.appendChild(c);
-        a.appendChild(d);
-        e.appendChild(a);
-        i3++;
-        if (i == nextRow) {
-            i2++;
-            i3 = 1;
-            nextRow += 5;
-        }
-    }
+
+    $('#pokecards').empty();
+    pokemons = response.data.results;
+
+    paginate(currentPage, PAGE_SIZE, pokemons);
+
+    $('body').on('click', '.pokecard', async function (e) {
+        const pokemonName = $(this).attr('pokeName');
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const types = res.data.types.map((type) => type.type.name);
+        $('modal-body').html(`
+            <div style='width: 200px;'>
+            <img src='${response.data.sprites.other['official-artwork'].front_default}' alt='${response.data.name}'>
+                <div>
+                <h3>Abilities</h3>
+                <ul>
+                ${res.data.abilities.map((ability) => `<li>${ability.ability.name}</li>`).join('')}
+                </ul>
+                </div>
+            
+                <div>
+                <h3>Stats</h3>
+                <ul>
+                ${response.data.stats.map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`).join('')}
+                </ul>
+                </div>
+            </div>
+
+            <h3>Types</h3>
+            <ul>
+            ${types.map((type) => `<li>${type}</li>`).join('')}
+            </ul>
+        `);
+        $(".modal-title").html(`
+            <h2>${response.data.name.toUpperCase()}</h2>
+            <h5>${response.data.id}</h5>
+        `)
+    });
+
+    $("body").on("click", "numberedbuttons", async function (e) {
+        currentPage = Number(e.target.value);
+        paginate(currentPage, PAGE_SIZE, pokemons);
+    });
+    
     document.getElementById("title").innerHTML = "Pokemon!";
 }
 
